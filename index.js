6f401237c706a3aedfa044e2d766e59c19e123f5
Kent Andersen <kentareandersen@gmail.com>
2016-01-26 14:39:21 +0100

    changed to use cheerio

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 let fs = require('fs');
-let jsdom = require('node-jsdom');
+let cheerio = require('cheerio');
+var fetch = require('node-fetch');
 let Duration = require('duration');
 let dateFormat = require('dateformat');
 let ProgressBar = require('progress');
@@ -18,23 +19,20 @@ function appendListOfPages(data) {
 }
 
 function getDocumentForUrl(url) {
-  return new Promise((resolve, reject) => {
-    jsdom.env(url, (e, w) => {
-      e ? reject(e) : resolve(w.document, w);
-      w.close();
-    });
-  });
+  return fetch(url)
+          .then(resp => resp.text())
+          .then(html => cheerio.load(html));
 }
 
 function checkPage(url) {
-  return getDocumentForUrl(url).then((document, window)=> {
-    if(document.querySelector('table:not(.table)')) {
-      let title = document.head.querySelector('title').textContent;
-      return appendListOfPages(`${title}\n${url}\n\n`).then(() => window);
+  return getDocumentForUrl(url).then($ => {
+    // there is a bug when using the :not selector with context
+    // https://github.com/fb55/css-select/issues/21
+    if($('table').filter(':not(.table)').length) {
+      let title = $('title').text().trim();
+      return appendListOfPages(`${title}\n${url}\n\n`)
     }
-
-    return Promise.resovle(window);
-  }).then(w => w.close());
+  });
 }
 
 
@@ -53,7 +51,7 @@ appendListOfPages(`\n\n${dateFormat(new Date(), 'mmmm dS yyyy, HH:MM')}\n-------
     });
 
     let checkNextPage = () => {
-      if(sitemap.length === 0) {
+      if(!sitemap.length) {
         return;
       }
 
@@ -71,5 +69,5 @@ appendListOfPages(`\n\n${dateFormat(new Date(), 'mmmm dS yyyy, HH:MM')}\n-------
   .catch(console.log.bind(console));
 
 process.on('exit', () =>
-  console.log(`Matching took ${new Duration(startTime, new Date()).toString(1, 1)}`)
+  console.log(`\nMatching took ${new Duration(startTime, new Date()).toString(1, 1)}`)
 );
